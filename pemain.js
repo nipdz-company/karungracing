ea30f4885ebb2988f4be4ad0eb1ebe9b58a78255
Dhiyaul-n <140733237+Dhiyaul-n@users.noreply.github.com>
2023-09-18 15:39:42 +0700

    Add files via upload

---
+++ pemain.js
@@ -0,0 +1,28 @@
+class pemain{
+  constructor(y,warna){
+    this.d = 40
+    this.warna = color(warna)
+    this.x = 50
+    this.y = y
+    this.speed = 10
+  }
+  
+  tampilkan(){
+    fill(this.warna)
+    circle(this.x, this.y,this.d)
+  }
+  
+  maju(){
+    this.x += this.speed
+  }
+  finish(){
+    if(this.x>width-100) {
+      fill(this.warna)
+      textSize(50)
+      textAlign(CENTER)
+      text('FINISH', width/2 , height/2)
+      noLoop()
+    }
+  }
+  
+}
---
+++ sketch.js
@@ -0,0 +1,39 @@
+let x,y
+let pemain1, pemain2
+function garis(x1, y1){
+  line(x1, y1)
+}
+
+function garis(x,y){
+  line(x,y,x, height-y)
+}
+
+
+function setup(){
+  createCanvas(600,400);
+  
+  pemain1 = new pemain(100, 'red')
+  pemain2 = new pemain(300, 'blue')
+}
+
+function draw(){
+  background(1000,1000,1000)
+  
+  garis(100,50)
+  garis(width-100,50)
+  
+  pemain1.tampilkan()
+  pemain2.tampilkan()
+  
+  pemain1.finish()
+  pemain2.finish()
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    pemain1.maju()
+  }
+  else if(keyCode === RIGHT_ARROW){
+    pemain2.maju()
+  }
+}
